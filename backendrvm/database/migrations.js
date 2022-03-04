be4da902f7384842f0ecfe43466dcf1a41b8046b
Elvis <elvissamir18@gmail.com>
2022-03-04 18:42:58 -0400

    Added data for genres model in backend migrations.

--- backendrvm/database/migrations.js
+++ backendrvm/database/migrations.js
@@ -1,23 +1,36 @@
 const { Genre } = require('../models/Genre')
 
-const genres = []
+const genres = [
+    { name: 'Adventure'},
+    { name: 'Romance' },
+    { name: 'Drama' },
+    { name: 'Action' },
+    { name: 'Animation' },
+    { name: 'Thriller' },
+    { name: 'Comedy' },
+    { name: 'Western' },
+    { name: 'Sci-fi' },
+    { name: 'Documentary' },
+]
 
 const migrations = [
     { name: 'Genres', model: Genre, data: genres },
 ]
 
-const doMigration = async () => {
-    return 
+const doMigration = async ({ model, data }) => {
+    await model.collection.insertMany(data)
 }
 
 const runMigrations = async () => {
     for (const migration of migrations) {
         try {
-            await doMigration()
+            console.log(`Running migration for ${migration.name} Model...`)
+            await doMigration(migration)
+            console.log(`Finished migration for ${migration.name} Model.`)
         }
         catch (ex)
         {
-            console.log()
+            console.log(`There was an error with migration for ${migration.name} model.`, ex)
         }
     }
 }
