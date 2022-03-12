8ebd46476687fd37821b2efdf7eb184540bcc7df
Elvis <elvissamir18@gmail.com>
2022-03-12 06:00:14 -0400

    Added sort by column feature to Customers Component.

--- frontend/src/components/Customers.jsx
+++ frontend/src/components/Customers.jsx
@@ -1,36 +1,56 @@
+import _ from 'lodash';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { getCustomers } from '../services/customersService';
+import { paginate } from '../utils/paginate';
 import CustomersTable from './CustomersTable'
 
 function Customers () {
-
-    // SET DATA
     const [ customers, setCustomers ] = useState([])
+    const [ pageSize, setPageSize ] = useState(4)
+    const [ currentPage, setCurrentPage ] = useState(1)
     const [ sortColumn, setSortColumn ] = useState({ path: 'name', order: 'asc' })
 
-    // FETCH DATA
     useEffect(() => {
         const fetchCustomers = async () => {
             const { data } = await getCustomers()
-            console.log(data)
             setCustomers(data)
         }
 
         fetchCustomers()
     }, [])
 
-    // SHOW DATA
+    const sortCustomersList = (list) => {
+        if (sortColumn.path === 'name')
+            return _.orderBy(list, customer => customer.first_name, sortColumn.order)
+        
+        return _.orderBy(list, sortColumn.path, sortColumn.order)
+    }
+
+    const getPagedData = () => {
+        // copy customers
+        const list = [ ...customers ]
+
+        // sort
+        const sortedList = sortCustomersList(list)
+
+        // paginate
+        const paginatedList = paginate(sortedList, currentPage, pageSize)
+
+        return { data: paginatedList, totalCount: customers.length }
+    }
+
+    const { data, totalCount } = getPagedData()
 
     // HANDLE DATA
-    const handleSort = () => {
-
+    const handleSort = sortColumn => {
+        setSortColumn(sortColumn)
     }
     
     return (
         <div className='mt-8'>
             <CustomersTable 
-                customers={ customers }
+                customers={ data }
                 sortColumn={ sortColumn }
                 onSort={ handleSort }></CustomersTable>
         </div>
